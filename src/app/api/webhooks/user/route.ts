c5aa406130d2b7d6d9efe56232b566dac3d43cd4
Amir-Adel9 <amiradel7152@gmail.com>
2023-10-01 03:25:43 +0300

    Added user registration webhook

---
+++ src/app/api/webhooks/user/route.ts
@@ -0,0 +1,66 @@
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { db } from '@/db';
+import { user } from '@/db/schema';
+
+export async function POST(req: Request) {
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
+    );
+  }
+
+  const headerPayload = headers();
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400,
+    });
+  }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  try {
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400,
+    });
+  }
+
+  // Get the ID and type
+  const { id, ...attributes } = evt.data;
+  const eventType = evt.type;
+
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log('Webhook body:', body);
+
+  if (eventType === 'user.created') {
+    console.log('User created webhook received');
+    console.log('User ID:', evt.data.id);
+    console.log('User Info', attributes);
+
+    await db.insert(user).values({
+      clerkId: evt.data.id,
+      username: evt.data.username,
+    });
+  }
+
+  return new Response('', { status: 201 });
+}
