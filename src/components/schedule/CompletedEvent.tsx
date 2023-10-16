79a33564eb8aea739ab18a5a4bb33d7401e32c34
Amir-Adel9 <amiradel7152@gmail.com>
2023-10-17 01:45:53 +0300

    Completed schedule page

---
+++ src/components/schedule/CompletedEvent.tsx
@@ -0,0 +1,151 @@
+import { forwardRef } from 'react';
+
+import Image from 'next/image';
+
+import { Event, MatchTeam } from '@/utils/constants/types';
+import Link from 'next/link';
+
+const CompletedEvent = forwardRef(
+  ({
+    event,
+    startTime,
+    teams,
+  }: {
+    event: Event;
+    startTime: {
+      hour: string;
+      minute: string;
+    };
+    teams: {
+      firstTeam: MatchTeam;
+      secondTeam: MatchTeam;
+    };
+  }) => {
+    return (
+      <div className='relative w-full h-[120px] bg-accent-blue text-primary px-6 py-4 font-kanit border-y-4 border-accent-gold group duration-300 hover:bg-[#0b2c38]'>
+        <Link
+          href={`/match/${event.match.id}`}
+          className='w-full flex items-center justify-between'
+        >
+          <div className='relative'>
+            <span className='text-xl sm:text-3xl'>{startTime.hour}</span>
+            <span className='absolute text-xs sm:text-sm top-1 ml-1'>
+              {startTime.minute}
+            </span>
+          </div>
+          <div className='flex justify-center flex-grow items-center flex-col lg:flex-row gap-1 lg:gap-0'>
+            <div className='flex flex-row lg:flex-row-reverse w-24 lg:w-1/3 items-center justify-between lg:justify-start  space-x-1 lg:gap-4 '>
+              <Image
+                src={teams.firstTeam.image}
+                alt={teams.firstTeam.name}
+                width={80}
+                height={80}
+                className={`w-10 h-10 lg:w-16 lg:h-16 xl:w-20 xl:h-20  ${
+                  teams.firstTeam.result.outcome === 'loss'
+                    ? 'opacity-30 group-hover:opacity-100 duration-300'
+                    : ''
+                }`}
+                draggable={false}
+              />
+              <div className='flex flex-col lg:items-end flex-grow lg:flex-grow-0'>
+                <div className='flex items-center justify-between lg:justify-normal'>
+                  <h3 className='hidden lg:inline text-end'>
+                    {teams.firstTeam.name}
+                  </h3>
+                  <h3
+                    className={`inline lg:hidden font-semibold lg:font-normal ${
+                      teams.firstTeam.result.outcome === 'win'
+                        ? 'lg:text-accent-gold '
+                        : ''
+                    }`}
+                  >
+                    {teams.firstTeam.code}
+                  </h3>
+                  <span
+                    className={`inline lg:hidden font-bold ${
+                      teams.firstTeam.result.outcome === 'win'
+                        ? 'text-accent-gold'
+                        : ''
+                    }`}
+                  >{`${teams.firstTeam.result.gameWins}`}</span>
+                </div>
+                <span className='  font-bold text-xs text-end hidden lg:inline'>
+                  {teams.firstTeam.record.wins}W -{' '}
+                  {teams.firstTeam.record.losses}L
+                </span>
+              </div>
+            </div>
+            <div className='font-bold px-8 xl:px-14 hidden lg:inline'>
+              <span
+                className={`relative ${
+                  teams.firstTeam.result.outcome === 'win'
+                    ? 'before:content-["◀"] before: before:text-accent-gold before:text-xs'
+                    : ''
+                }`}
+              >
+                {` ${teams.firstTeam.result.gameWins} `}
+              </span>
+              <span>- </span>
+              <span
+                className={`relative ${
+                  teams.secondTeam.result.outcome === 'win'
+                    ? 'after:content-["▶"]  after:text-accent-gold after:text-xs'
+                    : ''
+                }`}
+              >
+                {`${teams.secondTeam.result.gameWins} `}
+              </span>
+            </div>
+            <div className='flex flex-row lg:flex-row w-24 lg:w-1/3 items-center justify-between lg:justify-start space-x-1 lg:space-x-4'>
+              <Image
+                src={teams.secondTeam.image}
+                alt={teams.secondTeam.name}
+                width={80}
+                height={80}
+                className={`w-10 h-10 lg:w-16 lg:h-16 xl:w-20 xl:h-20  ${
+                  teams.secondTeam.result.outcome === 'loss'
+                    ? 'opacity-30 group-hover:opacity-100 duration-300'
+                    : ''
+                }`}
+                draggable={false}
+              />
+              <div className='flex flex-col lg:items-start flex-grow lg:flex-grow-0 '>
+                <div className='flex items-center justify-between lg:justify-normal '>
+                  <h3 className='hidden lg:inline'>{teams.secondTeam.name}</h3>
+                  <h3
+                    className={`inline lg:hidden font-semibold lg:font-normal ${
+                      teams.secondTeam.result.outcome === 'win'
+                        ? 'lg:text-accent-gold '
+                        : ''
+                    }`}
+                  >
+                    {teams.secondTeam.code}
+                  </h3>
+                  <span
+                    className={`inline lg:hidden font-bold ${
+                      teams.secondTeam.result.outcome === 'win'
+                        ? 'text-accent-gold'
+                        : ''
+                    }`}
+                  >{`${teams.secondTeam.result.gameWins}`}</span>
+                </div>
+                <span className='font-bold text-xs text-end hidden lg:inline'>
+                  {teams.secondTeam.record.wins}W -{' '}
+                  {teams.secondTeam.record.losses}L
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className='font-bold flex-col hidden xs:flex'>
+            <span> {event.league.name} </span>
+            <span className=' hidden lg:inline'>{`Best of ${event.match.strategy.count}`}</span>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+);
+
+CompletedEvent.displayName = 'CompletedEvent';
+
+export default CompletedEvent;
---
+++ src/components/schedule/LiveEvent.tsx
@@ -0,0 +1,87 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Event, MatchTeam } from '@/utils/constants/types';
+
+const LiveEvent = ({
+  event,
+  teams,
+}: {
+  event: Event;
+  teams: {
+    firstTeam: MatchTeam;
+    secondTeam: MatchTeam;
+  };
+}) => {
+  return (
+    <div className='relative w-full h-[120px] bg-secondary text-primary px-6 py-4 font-kanit border-y-4 border-red-700 animate-bounce-y group duration-300 hover:bg-[#0b2c38]'>
+      <Link
+        href='https://www.twitch.tv/lolesports_ar'
+        target={'_blank'}
+        className='relative w-full h-full flex items-center justify-between'
+      >
+        <div className='absolute top-1 left-0 ml-2 flex items-center justify-center gap-x-1'>
+          <svg
+            width='16px'
+            height='16px'
+            viewBox='0 0 15 15'
+            version='1.1'
+            id='circle'
+            xmlns='http://www.w3.org/2000/svg'
+            className='fill-red-700'
+          >
+            <path d='M14,7.5c0,3.5899-2.9101,6.5-6.5,6.5S1,11.0899,1,7.5S3.9101,1,7.5,1S14,3.9101,14,7.5z' />
+          </svg>
+          <span className='font-bold'>Live</span>
+        </div>
+        <div className='h-full flex justify-center flex-grow items-center flex-col lg:flex-row gap-1 lg:gap-0'>
+          <div className='h-full flex flex-row lg:flex-row-reverse w-24 lg:w-1/3 items-center justify-between lg:justify-start  space-x-1 lg:gap-4 '>
+            <Image
+              src={teams.firstTeam.image}
+              alt={teams.firstTeam.name}
+              width={80}
+              height={80}
+              className='w-10 h-10 lg:w-16 lg:h-16 xl:w-20 xl:h-20'
+              draggable={false}
+            />
+            <div className='flex flex-col lg:items-end flex-grow lg:flex-grow-0'>
+              <div className='flex items-center justify-between lg:justify-normal'>
+                <h3 className='hidden lg:inline text-end'>
+                  {teams.firstTeam.name}
+                </h3>
+                <h3 className='inline lg:hidden font-semibold lg:font-normal'>
+                  {teams.firstTeam.code}
+                </h3>
+              </div>
+            </div>
+          </div>
+          <h3 className='hidden lg:inline font-bold lg:px-8 xl:px-14'>VS</h3>
+          <div className='flex flex-row lg:flex-row w-24 lg:w-1/3 items-center justify-between lg:justify-start space-x-1 lg:space-x-4'>
+            <Image
+              src={teams.secondTeam.image}
+              alt={teams.secondTeam.name}
+              width={80}
+              height={80}
+              className='w-10 h-10 lg:w-16 lg:h-16 xl:w-20 xl:h-20'
+              draggable={false}
+            />
+            <div className='flex flex-col lg:items-start flex-grow lg:flex-grow-0 '>
+              <div className='flex items-center justify-between lg:justify-normal '>
+                <h3 className='hidden lg:inline'>{teams.secondTeam.name}</h3>
+                <h3 className='inline lg:hidden font-semibold lg:font-normal'>
+                  {teams.secondTeam.code}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='font-bold flex-col hidden xs:flex'>
+          <span> {event.league.name} </span>
+          <span className=' hidden lg:inline'>{`Best of ${event.match.strategy.count}`}</span>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default LiveEvent;
