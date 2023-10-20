5894beb42801509dc72d28292ea51404b442bb8f
Amir-Adel9 <amiradel7152@gmail.com>
2023-10-20 12:09:31 +0300

    Fixed build issue

--- src/app/api/standings/route.ts
+++ src/app/api/standings/route.ts
@@ -3,10 +3,10 @@ import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 // export const runtime = `${
-      process.env.NEXT_PUBLIC_ENVIRONMENT !== 'DEVELOPMENT'
-        ? 'https://arabian-league-grounds-enhanced.vercel.app/'
-        : 'http://localhost:3002/'
-    }api/schedule/upcoming-events`;
+//       process.env.NEXT_PUBLIC_ENVIRONMENT !== 'DEVELOPMENT'
+//         ? 'https://arabian-league-grounds-enhanced.vercel.app/'
+//         : 'http://localhost:3002/'
+//     }api/schedule/upcoming-events`;
 // export const cache = 'no-store';
 
 export async function GET(_request: NextRequest) {
