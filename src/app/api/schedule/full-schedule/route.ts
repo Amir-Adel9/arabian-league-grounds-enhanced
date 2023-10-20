5b52e49a0c30a6254e9c9b0f5a54b818dc4d6392
Amir-Adel9 <amiradel7152@gmail.com>
2023-10-20 11:34:36 +0300

    Moved the "full-schedule" route to the edge

--- src/app/api/schedule/full-schedule/route.ts
+++ src/app/api/schedule/full-schedule/route.ts
@@ -3,6 +3,8 @@ import { Event } from '@/utils/constants/types';
 import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export async function GET(_request: NextRequest) {
   const fullSchedule: Promise<Event[]> = await fetch(
     `https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-US&leagueId=${process.env.NEXT_PUBLIC_LEAGUE_ID}`,
@@ -13,20 +15,20 @@ export async function GET(_request: NextRequest) {
       return matches.data.schedule.events;
     });
 
-  revalidatePath(_request.url);
-  revalidatePath(
-    `${
-      process.env.NEXT_PUBLIC_ENVIRONMENT !== 'DEVELOPMENT'
-        ? 'https://arabian-league-grounds-enhanced.vercel.app/'
-        : 'http://localhost:3002/'
-    }api/schedule/full-schedule`
-  );
-  revalidatePath(
-    `${
-      process.env.NEXT_PUBLIC_ENVIRONMENT !== 'DEVELOPMENT'
-        ? 'https://arabian-league-grounds-enhanced.vercel.app/'
-        : 'http://localhost:3002/'
-    }schedule`
-  );
+  // revalidatePath(_request.url);
+  // revalidatePath(
+  //   `${
+  //     process.env.NEXT_PUBLIC_ENVIRONMENT !== 'DEVELOPMENT'
+  //       ? 'https://arabian-league-grounds-enhanced.vercel.app/'
+  //       : 'http://localhost:3002/'
+  //   }api/schedule/full-schedule`
+  // );
+  // revalidatePath(
+  //   `${
+  //     process.env.NEXT_PUBLIC_ENVIRONMENT !== 'DEVELOPMENT'
+  //       ? 'https://arabian-league-grounds-enhanced.vercel.app/'
+  //       : 'http://localhost:3002/'
+  //   }schedule`
+  // );
   return NextResponse.json(fullSchedule);
 }
