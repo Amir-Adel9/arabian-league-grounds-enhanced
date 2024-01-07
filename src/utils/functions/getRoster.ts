1e74068c2b77d41714f8d1ecb835f4faef8ae0f3
Amir-Adel9 <amiradel7152@gmail.com>
2024-01-07 08:37:03 +0200

    fixed build error

--- src/utils/functions/getRoster.ts
+++ src/utils/functions/getRoster.ts
@@ -1,16 +1,16 @@
-import { requestParams } from "../constants/requestParams";
-import { Event, Stats, Team } from "../types/types";
-import { getPostEventStats } from "./getPostEventStats";
+import { requestParams } from '../constants/requestParams';
+import { Event, Stats, Team } from '../types/types';
+import { getPostEventStats } from './getPostEventStats';
 
 export async function getRoster(slugs: string[]) {
   const allCompletedEvents = await fetch(
     `https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-US&leagueId=${process.env.NEXT_PUBLIC_LEAGUE_ID}`,
-    { ...requestParams, cache: "no-cache" }
+    { ...requestParams, cache: 'no-cache' }
   )
     .then((res) => res.json())
     .then((res) => res.data.schedule.events)
     .then((events) =>
-      events.filter((event: Event) => event.state === "completed")
+      events.filter((event: Event) => event.state === 'completed')
     );
 
   const latestCompletedMatchForTeams: Event[] = await Promise.all(
@@ -40,13 +40,9 @@ export async function getRoster(slugs: string[]) {
     })
   );
   const rosters = latestCompletedMatchForTeams.forEach(async (event) => {
-    const x: Stats[] | "Event has not concluded yet" = await getPostEventStats({
+    const x: Stats[] = await getPostEventStats({
       event,
     });
-
-    if (x === "Event has not concluded yet") {
-      return x;
-    }
 
     // console.log(x[0]);
 
