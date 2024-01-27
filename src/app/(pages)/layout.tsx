2b1010bb51223ff73df0064bb3a2a882c86b6250
Amir-Adel9 <amiradel7152@gmail.com>
2024-01-27 07:23:15 +0200

    wink wink

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -17,6 +17,7 @@ import { Toaster } from 'react-hot-toast';
 import { fulfillPredictions } from '@/entities/prediction/actions/fulfillPredictions';
 import { getRoster } from '@/utils/functions/getRoster';
 import { fulfillUpdates } from '@/entities/actions';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const rubik = Rubik({
@@ -57,29 +58,52 @@ export default async function RootLayout({
   matchModal: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang='en' className='bg-background'>
-        <head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-          <link
-            href='https://fonts.googleapis.com/css2?family=Gluten:wght@100;200;300;400;500;600;700;800;900&display=swap'
-            rel='stylesheet'
-          />
-        </head>
-        <body
-          className={`${GeistSans.variable} ${GeistMono.variable} ${inter.variable} ${rubik.variable} ${gluten.variable} ${permanentMarker.variable} ${kanit.variable}`}
-        >
-          {/*  @ts-ignore Async Server Component */}
-          <Header />
-          <Navbar />
-          {children}
-          {matchModal}
-          {/* <Footer /> */}
-          <Toaster position='top-center' reverseOrder={false} />
-        </body>
-      </html>
-    </ClerkProvider>
+    // <ClerkProvider>
+    //   <html lang='en' className='bg-background'>
+    //     <head>
+    //       <link rel='preconnect' href='https://fonts.googleapis.com' />
+    //       <link rel='preconnect' href='https://fonts.gstatic.com' />
+    //       {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+    //       <link
+    //         href='https://fonts.googleapis.com/css2?family=Gluten:wght@100;200;300;400;500;600;700;800;900&display=swap'
+    //         rel='stylesheet'
+    //       />
+    //     </head>
+    //     <body
+    //       className={`${GeistSans.variable} ${GeistMono.variable} ${inter.variable} ${rubik.variable} ${gluten.variable} ${permanentMarker.variable} ${kanit.variable}`}
+    //     >
+    //       {/*  @ts-ignore Async Server Component */}
+    //       <Header />
+    //       <Navbar />
+    //       {children}
+    //       {matchModal}
+    //       {/* <Footer /> */}
+    //       <Toaster position='top-center' reverseOrder={false} />
+    //     </body>
+    //   </html>
+    // </ClerkProvider>
+    <section className='w-full min-h-screen relative flex flex-col justify-center items-center'>
+      <div className='absolute w-full h-full bg-primary opacity-80 z-[-10]'></div>
+      <Image
+        src='/images/background.jpg'
+        alt='Background Image'
+        className='w-full h-full z-[-20]'
+        layout='fill'
+        objectFit='cover'
+        draggable={false}
+        objectPosition='center'
+      />
+      <div className='text-4xl font-bold text-accent-gold relative w-full flex flex-col gap-5 justify-center items-center '>
+        <Image
+          src='/images/dinger.gif'
+          alt='dinger Image'
+          className='animate-bounce-y'
+          width={260}
+          height={260}
+          draggable={false}
+        />
+        ???
+      </div>
+    </section>
   );
 }
