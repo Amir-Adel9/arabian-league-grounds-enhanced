8fd0ebb9ee441801e36d5fd1fb88b3058a1fcaac
Amir-Adel9 <amiradel7152@gmail.com>
2024-02-13 07:03:56 +0200

    zzzz

--- src/app/api/update/route.ts
+++
@@ -1,8 +0,0 @@
-import { fulfillUpdates } from '@/entities/actions';
-
-export const dynamic = 'force-dynamic';
-
-export async function GET() {
-  await fulfillUpdates();
-  return new Response(`Success! Updates have been made`);
-}
